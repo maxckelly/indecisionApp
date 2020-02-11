ba85a0f2cc17a84dec895d849dff0bd379ae10d5
Max Kelly <max@revelthat.com>
2020-02-11 14:31:57 +1100

    styled components

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,9 +3,10 @@ import React from 'react';
 // Setup an options prop for the Options component
 // Render the length of the array
 const Option = (props) => (
-  <div>
-    {props.optionText}
-    <button
+  <div className="option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
+    <button 
+      className="button button--link"
       onClick={(e) => {
         props.handleDeleteOption(props.optionText)
       }}
--- src/components/Options.js
+++ src/components/Options.js
@@ -5,13 +5,22 @@ import Option from './Option.js';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}> Remove All </button>
-    {props.options.length === 0 && <p> Please add an option </p>}
+    <div className="widget-header">
+      <h3 className="widget-header__title"> Your Options </h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      > 
+        Remove All 
+      </button>
+    </div>
+    {props.options.length === 0 && <p className="widget-header__sub-title"> Please add an option </p>}
     {
-      props.options.map((option) => (
+      props.options.map((option, index) => (
         <Option
           key={option}
           optionText={option}
+          count={index + 1}
           handleDeleteOption={props.handleDeleteOption}
         />
       ))

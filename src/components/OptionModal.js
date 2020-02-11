559f86f56a83bf28093f8e15c29ad71626260f52
Max Kelly <max@revelthat.com>
2020-02-11 15:16:50 +1100

    added final styles

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -9,10 +9,11 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelectedOption}
     contentLabel="Selected Option"
+    className="modal"
   >
-    <h3> Selected Option: </h3>
-    {props.selectedOption && <p> {props.selectedOption} </p>}
-    <button onClick={props.handleClearSelectedOption}> Close </button>
+    <h3 className="modal__title"> Selected Option: </h3>
+    {props.selectedOption && <p className="modal__body"> {props.selectedOption} </p>}
+    <button className="button" onClick={props.handleClearSelectedOption}> Close </button>
   </Modal>
 );
 
